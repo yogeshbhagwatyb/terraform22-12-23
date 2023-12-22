9dec36f855a9b3851004e747c60a8cb38ea60131
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-12-22 15:51:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,5 +3,4 @@ backend "s3" {
     bucket = "jhooq-terraform-s3-buket"
     key = "jhooq/terraform/remote/s3/terraform.tfstate"
     region     = "us-east-1"
-    dynamodb_table = "dynamodb-state-locking"
 }
